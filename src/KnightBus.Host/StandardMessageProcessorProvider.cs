f67cce3aaad9bc84a988e3fac01b5d86f0f84930
niklasarbin <niklsa.arbin@bookbeat.com>
2018-10-24 21:37:35 +0200

    Add StandardMessageProcessorProvider Tests and support events

--- src/KnightBus.Host/StandardMessageProcessorProvider.cs
+++ src/KnightBus.Host/StandardMessageProcessorProvider.cs
@@ -10,15 +10,25 @@ namespace KnightBus.Host
     {
         private readonly ConcurrentDictionary<Type, object> _processors = new ConcurrentDictionary<Type, object>();
 
-        public StandardMessageProcessorProvider RegisterProcessor<T>(IProcessMessage<T> processor) where T : IMessage
+        public StandardMessageProcessorProvider RegisterProcessor(object processor)
         {
-            _processors[typeof(T)] = processor;
+            Register(processor, typeof(IProcessCommand<,>));
+            Register(processor, typeof(IProcessEvent<,,>));
             return this;
+        }
+
+        private void Register(object processor, Type type)
+        {
+            var processorInterfaces = ReflectionHelper.GetAllInterfacesImplementingOpenGenericInterface(processor.GetType(), type);
+            foreach (var processorInterface in processorInterfaces)
+            {
+                _processors[processorInterface] = processor;
+            }
         }
 
         public IProcessMessage<T> GetProcessor<T>(Type type) where T : IMessage
         {
-            return (IProcessMessage<T>) _processors[typeof(T)];
+            return (IProcessMessage<T>) _processors[type];
         }
 
         public IEnumerable<Type> ListAllProcessors()
