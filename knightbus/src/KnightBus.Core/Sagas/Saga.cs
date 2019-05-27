8addda67659b8d8bf69eff5a6aa3dcfb5da852d5
Niklas Arbin <niklas.arbin@bookbeat.com>
2019-05-27 16:29:41 +0200

    Test checkpoint

--- knightbus/src/KnightBus.Core/Sagas/Saga.cs
+++ knightbus/src/KnightBus.Core/Sagas/Saga.cs
@@ -26,7 +26,9 @@ namespace KnightBus.Core.Sagas
         /// Retrieve a mapping function for a message
         /// </summary>
         Func<IMessage, string> GetMapping<TMessage>() where TMessage : IMessage;
-        Func<IMessage, string> GetStartMapping<TMessage>() where TMessage : IMessage;
+
+        Type StartMessageType { get; }
+        ISagaStore SagaStore { get; set; }
     }
 
     public class Saga<T> : ISaga<T> where T : ISagaData
@@ -34,6 +36,7 @@ namespace KnightBus.Core.Sagas
         public T Data { get; set; }
         private Dictionary<Type, Func<IMessage, string>> _mappings = new Dictionary<Type, Func<IMessage, string>>();
         private Func<IMessage, string> _startMapping;
+        public ISagaStore SagaStore { get; set; }
 
         public void MapMessage<TMessage>(Func<TMessage, string> mapping) where TMessage : IMessage
         {
@@ -45,7 +48,8 @@ namespace KnightBus.Core.Sagas
 
         public void MapStartMessage<TMessage>(Func<TMessage, string> mapping) where TMessage : IMessage
         {
-            _startMapping = mapping as Func<IMessage, string>;
+            MapMessage<TMessage>(mapping);
+            StartMessageType = typeof(TMessage);
         }
 
         public Func<IMessage, string> GetMapping<TMessage>() where TMessage : IMessage
@@ -53,15 +57,7 @@ namespace KnightBus.Core.Sagas
             return _mappings[typeof(TMessage)];
         }
 
-        public Func<IMessage, string> GetStartMapping<TMessage>() where TMessage : IMessage
-        {
-            return _startMapping;
-        }
-    }
-
-    public interface IStartSaga
-    {
-
+        public Type StartMessageType { get; private set; }
     }
 
     public interface ISagaData
@@ -69,7 +65,7 @@ namespace KnightBus.Core.Sagas
 
     }
 
-    public interface ISagaStore<T> where T : ISagaData
+    public interface ISagaStore 
     {
         Task<T> GetSaga<T>(string id);
         Task<T> Create<T>(string id, T sagaData);
@@ -98,19 +94,11 @@ namespace KnightBus.Core.Sagas
             //Find Saga or create one 
             if (processor is ISaga)
             {
-                var id = "";
                 var sagaType = ReflectionHelper.GetAllInterfacesImplementingOpenGenericInterface(processor.GetType(), typeof(ISaga<>)).Single();
                 var sagaDataType = sagaType.GenericTypeArguments[0];
 
                 var sagaHandlerType = typeof(SagaHandler<,>).MakeGenericType(sagaDataType, typeof(T));
                 var sagaHandler = Activator.CreateInstance(sagaHandlerType, _sagaStore, processor, await messageStateHandler.GetMessageAsync().ConfigureAwait(false));
-
-                var sagaData = await _sagaStore.GetSaga(sagaDataType, id).ConfigureAwait(false);
-                if (typeof(IStartSaga).IsAssignableFrom(typeof(T)))
-                {
-                    //Saga should start
-                    await _sagaStore.Create(sagaDataType, id);
-                }
             }
 
             await next.ProcessAsync(messageStateHandler, cancellationToken).ConfigureAwait(false);
@@ -119,11 +107,11 @@ namespace KnightBus.Core.Sagas
 
     public class SagaHandler<TSagaData, TMessage> where TSagaData : ISagaData, new() where TMessage : IMessage
     {
-        private readonly ISagaStore<TSagaData> _sagaStore;
+        private readonly ISagaStore _sagaStore;
         private readonly ISaga<TSagaData> _saga;
         private readonly TMessage _message;
 
-        public SagaHandler(ISagaStore<TSagaData> sagaStore, ISaga<TSagaData> saga, TMessage message)
+        public SagaHandler(ISagaStore sagaStore, ISaga<TSagaData> saga, TMessage message)
         {
             _sagaStore = sagaStore;
             _saga = saga;
@@ -132,10 +120,11 @@ namespace KnightBus.Core.Sagas
 
         public async Task Initialize()
         {
+            _saga.SagaStore = _sagaStore;
             TSagaData sagaData;
             var id = _saga.GetMapping<TMessage>().Invoke(_message);
 
-            if (typeof(IStartSaga).IsAssignableFrom(typeof(TMessage)))
+            if (_saga.StartMessageType == typeof(TMessage))
             {
                 sagaData = await _sagaStore.Create<TSagaData>(id, new TSagaData());
             }
@@ -146,31 +135,5 @@ namespace KnightBus.Core.Sagas
 
             _saga.Data = sagaData;
         }
-    }
-
-
-
-
-    public class SampleSaga : Saga<SampleSagaData>, IProcessMessage<BeginEncoding>
-    {
-        public SampleSaga()
-        {
-            MapMessage<BeginEncoding>(m => m.Id.ToString());
-        }
-
-        public Task ProcessAsync(BeginEncoding message, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class SampleSagaData : ISagaData
-    {
-        public string Message { get; set; }
-    }
-
-    public class BeginEncoding : IMessage
-    {
-        public Guid Id { get; set; }
     }
 }
---
+++ knightbus/tests/KnightBus.Core.Tests.Unit/SagaHandlerTests.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using KnightBus.Core.Sagas;
+using KnightBus.Messages;
+using Moq;
+using NUnit.Framework;
+
+namespace KnightBus.Core.Tests.Unit
+{
+    [TestFixture]
+    public class SagaHandlerTests
+    {
+        [Test]
+        public async Task Initialize_should_start_new_saga_when_start_message_is_received()
+        {
+            //arrange
+            var id = "something-unique";
+            var store = new Mock<ISagaStore>();
+            var saga = new TestSaga();
+            var startMessage = new TestSagaStartMessage(id);
+            var handler = new SagaHandler<TestSagaData, TestSagaStartMessage>(store.Object, saga, startMessage);
+            //act
+            await handler.Initialize();
+            //assert
+            store.Verify(x=> x.Create(id, It.IsAny<TestSagaData>()), Times.Once);
+        }
+
+        internal class TestSagaData : ISagaData
+        {
+        }
+        internal class TestSaga : Saga<TestSagaData>
+        {
+            public TestSaga()
+            {
+                MapStartMessage<TestSagaStartMessage>(m => m.MessageId);
+            }
+        }
+        internal class TestSagaStartMessage : IMessage
+        {
+            public TestSagaStartMessage(string id)
+            {
+                MessageId = id;
+            }
+            public string MessageId { get;} 
+        }
+    }
+}
