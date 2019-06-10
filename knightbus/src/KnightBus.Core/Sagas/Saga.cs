77e2fd21b53047afb1d40392eeb66df5926b0f81
Niklas Arbin <niklas.arbin@bookbeat.com>
2019-06-10 16:10:55 +0200

    Checkpoint

--- knightbus/src/KnightBus.Core/Sagas/Saga.cs
+++ knightbus/src/KnightBus.Core/Sagas/Saga.cs
@@ -5,7 +5,7 @@ namespace KnightBus.Core.Sagas
 {
     public interface ISaga
     {
-
+        ISagaMessageMapper MessageMapper { get; }
     }
     public interface ISaga<T> : ISaga where T : ISagaData
     {
@@ -13,30 +13,47 @@ namespace KnightBus.Core.Sagas
         /// Stateful data associated with the Saga
         /// </summary>
         T Data { get; set; }
-        ISagaMessageMapper MessageMapper { get; }
+
         ISagaStore SagaStore { get; set; }
+        Task CompleteAsync();
+        Task UpdateAsync(T sagaData);
+        Task FailAsync();
     }
 
-    public class Saga<T> : ISaga<T> where T : ISagaData
+    public abstract class Saga<T> : ISaga<T> where T : ISagaData
     {
-
         public T Data { get; set; }
         public ISagaMessageMapper MessageMapper { get; } = new SagaMessageMapper();
         public ISagaStore SagaStore { get; set; }
+        public virtual Task CompleteAsync()
+        {
+            return SagaStore.Complete(Data.Id, Data.Key);
+        }
+
+        public virtual Task UpdateAsync(T sagaData)
+        {
+            return SagaStore.Update(Data.Id, sagaData);
+        }
+
+        public virtual Task FailAsync()
+        {
+            return SagaStore.Fail(Data.Key, Data.Id);
+        }
     }
 
     public interface ISagaData
     {
+        string Id { get; }
         string Key { get; }
     }
 
     public interface ISagaStore 
     {
-        Task<T> GetSaga<T>(string id);
+        Task<T> GetSaga<T>(string id, string key);
         Task<T> Create<T>(string id, T sagaData);
-        Task Update<T>(T saga);
-        Task Complete(string id);
-        Task Fail(string id);
+        Task Update<T>(string id, T sagaData);
+        Task Complete(string key, string id);
+        Task Fail(string key, string id);
     }
 
     public class SagaAlreadyStartedException : Exception { }
