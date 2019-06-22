2fe43f766f8db6cee68e59ccf8181a2f924e6c4f
Niklas Arbin <arbin@kth.se>
2019-06-22 10:33:18 +0200

    Add Sql Server Saga Store (#11)
    * Inital commit for SqlServerSagaStore
    
    * Add db creation for integration tests
    
    * Finalize the tests and add sql store extension
    
    * move project to disk location
    
    * remove old .sln files
    
    * update build with new tests

---
+++ knightbus-sqlserver/tests/KnightBus.SqlServer.Tests.Integration/DatabaseInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace KnightBus.SqlServer.Tests.Integration
+{
+    [SetUpFixture]
+    public class DatabaseInitializer
+    {
+        private static string DatabaseName = "KnightBus";
+        public static readonly string ConnectionString = $@"Server=(local)\SQL2014;Database={DatabaseName};User ID=sa;Password=Password12!";
+        private const string AdminConnectionString = @"Server=(local)\SQL2014;Database=master;User ID=sa;Password=Password12!";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            using (var connection = new SqlConnection(AdminConnectionString))
+            {
+                connection.Open();
+                var sql = $@"IF DB_ID (N'{DatabaseName}') IS NOT NULL
+                            DROP DATABASE {DatabaseName};
+                            CREATE DATABASE {DatabaseName};";
+                var command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            using (var connection = new SqlConnection(AdminConnectionString))
+            {
+                connection.Open();
+                var sql = $@"
+                            IF DB_ID (N'{DatabaseName}') IS NOT NULL
+                            DROP DATABASE {DatabaseName};";
+                var command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
