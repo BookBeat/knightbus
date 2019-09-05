42fb6dc73d6ef27def86384c9f47abaab053a8af
Niklas Arbin <niklas.arbin@bookbeat.com>
2019-09-05 14:14:10 +0200

    Move try catch to catch all errors

--- knightbus-schedule/src/KnightBus.Schedule/JobExecutor.cs
+++ knightbus-schedule/src/KnightBus.Schedule/JobExecutor.cs
@@ -24,32 +24,32 @@ namespace KnightBus.Schedule
 
         async Task IJob.Execute(IJobExecutionContext context)
         {
-            //Try and get the lock
-            var lockId = typeof(T).FullName;
-            var lockHandle = await _lockManager.TryLockAsync(lockId, TimeSpan.FromSeconds(60), CancellationToken.None).ConfigureAwait(false);
-
-            if (lockHandle == null)
+            try
             {
-                //someone else has locked this instance, do nothing
-                return;
-            }
-            _logger.Information("Executing trigger {TriggerSetting} {LockHandle}", lockId, lockHandle);
+                var schedule = typeof(T).FullName;
+                var lockHandle = await _lockManager.TryLockAsync(schedule, TimeSpan.FromSeconds(60), CancellationToken.None).ConfigureAwait(false);
 
-            using (new SingletonTimerScope(_logger, lockHandle, false))
-            {
-                using (_dependencyInjection.GetScope())
+                if (lockHandle == null)
                 {
-                    try
+                    //someone else has locked this instance, do nothing
+                    return;
+                }
+
+                _logger.Information("Executing schedule {Schedule} {LockHandle}", schedule, lockHandle);
+
+                using (new SingletonTimerScope(_logger, lockHandle, false))
+                {
+                    using (_dependencyInjection.GetScope())
                     {
                         var processor = _dependencyInjection.GetInstance<IProcessSchedule<T>>();
                         await processor.ProcessAsync(context.CancellationToken).ConfigureAwait(false);
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Error processing trigger {typeof(T)} {e.Message} {e.StackTrace}");
-                        _logger.Error(e, "Error processing trigger {Trigger}", typeof(T));
-                    }
                 }
+            }
+            catch (Exception e)
+            {
+                ConsoleWriter.WriteLine($"Error processing schedule {typeof(T)} {e.Message} {e.StackTrace}");
+                _logger.Error(e, "Error processing schedule {Schedule}", typeof(T));
             }
         }
     }
