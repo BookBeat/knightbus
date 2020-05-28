c3e905ac01924253445a58d63ac59ea717876f4c
Viktor <viktor.hartenberger@bookbeat.com>
2020-05-28 14:21:38 +0200

    Throw SingletonLockManagerMissingException when wiring up KnightBus.Schedule without having a configured ISingletonLockManager.
    Also moved this exception to KnightBus.Core.Exceptions

--- knightbus/src/KnightBus.Host/TransportStarterFactory.cs
+++ knightbus/src/KnightBus.Host/TransportStarterFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using KnightBus.Core;
+using KnightBus.Core.Exceptions;
 using KnightBus.Core.Singleton;
 using KnightBus.Host.Singleton;
 using KnightBus.Messages;
@@ -40,7 +41,8 @@ namespace KnightBus.Host
         {
             if (typeof(ISingletonProcessor).IsAssignableFrom(type))
             {
-                if (_configuration.SingletonLockManager == null) throw new SingletonLockManagerMissingException();
+                if (_configuration.SingletonLockManager == null)
+                    throw new SingletonLockManagerMissingException("There is no ISingletonLockManager specified, you cannot use the ISingletonProcessor directive without one");
                 ConsoleWriter.WriteLine($"Setting {type.Name} in Singleton mode");
                 var singletonStarter = new SingletonChannelReceiver(channelReceiver, _configuration.SingletonLockManager, _configuration.Log);
                 return singletonStarter;
