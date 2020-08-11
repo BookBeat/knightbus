bd54117577e6ceca6b6c8a3bc5a5b2c1142a4591
Niklas Arbin <niklas.arbin@bookbeat.com>
2020-08-11 14:31:07 +0200

    make SagaTableData internal

--- knightbus-azurestorage/src/KnightBus.Azure.Storage/Sagas/SagaTableData.cs
+++ knightbus-azurestorage/src/KnightBus.Azure.Storage/Sagas/SagaTableData.cs
@@ -3,7 +3,7 @@ using Microsoft.WindowsAzure.Storage.Table;
 
 namespace KnightBus.Azure.Storage.Sagas
 {
-    public class SagaTableData : TableEntity
+    internal class SagaTableData : TableEntity
     {
         public string Json { get; set; }
         public DateTime? Expiration { get; set; }
