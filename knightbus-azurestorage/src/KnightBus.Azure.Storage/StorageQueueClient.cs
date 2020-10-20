682b350b54b713f602ab8c94ae36e4982a4cc485
Niklas Arbin <niklas.arbin@bookbeat.com>
2020-10-20 08:39:46 +0200

    Create the blob first and then add the message instead of trying to p… (#48)
    * Create the blob first and then add the message instead of trying to play with visibility. Reduces number of io operations from 3 to 2 per message
    
    * Simplify cleanup, and remove old comment

--- knightbus-azurestorage/src/KnightBus.Azure.Storage/StorageQueueClient.cs
+++ knightbus-azurestorage/src/KnightBus.Azure.Storage/StorageQueueClient.cs
@@ -117,27 +117,17 @@ namespace KnightBus.Azure.Storage
                 }
             }
 
-            //Create hidden queue message
             var cloudMessage = new CloudQueueMessage(_serializer.Serialize(storageMessage.Properties));
-            await _queue.AddMessageAsync(cloudMessage, TimeSpan.MaxValue, TimeSpan.FromDays(1), null, null).ConfigureAwait(false);
             try
             {
                 var blob = _container.GetBlockBlobReference(storageMessage.BlobMessageId);
                 await blob.UploadTextAsync(_serializer.Serialize(message)).ConfigureAwait(false);
-                //Make the queue message visible
-                await _queue.UpdateMessageAsync(cloudMessage, delay ?? TimeSpan.Zero, MessageUpdateFields.Visibility).ConfigureAwait(false);
+                await _queue.AddMessageAsync(cloudMessage, TimeSpan.MaxValue, delay ?? TimeSpan.Zero, null, null).ConfigureAwait(false);
             }
             catch (Exception)
             {
-                //If we cannot upload the blob the delete the hidden message and throw
-                try
-                {
-                    await _queue.DeleteMessageAsync(cloudMessage).ConfigureAwait(false);
-                }
-                catch (StorageException e) when (e.RequestInformation?.HttpStatusCode == 404)
-                {
-                    //Cannot erase what's not there
-                }
+                //If we cannot upload the blob or create the message try cleaning up and throw
+                await TryDeleteBlob(storageMessage.BlobMessageId).ConfigureAwait(false);
                 throw;
             }
         }
