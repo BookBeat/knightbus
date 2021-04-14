2343512ed4e9b5fa147587e4db6ccae16d306aa8
Niklas Arbin <niklas.arbin@bookbeat.com>
2021-04-14 10:57:48 +0200

    Dependency update (#68)
    * Update dependencies
    
    * net5.0
    
    * ignore attachments for json serialization
    
    * Add attachement tests for newtonsoft

--- knightbus-newtonsoft/src/KnightBus.Newtonsoft/NewtonsoftSerializer.cs
+++ knightbus-newtonsoft/src/KnightBus.Newtonsoft/NewtonsoftSerializer.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using KnightBus.Messages;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace KnightBus.Newtonsoft
 {
     public class NewtonsoftSerializer : IMessageSerializer
     {
+        private JsonSerializerSettings _settings;
+
+        public NewtonsoftSerializer()
+        {
+            _settings = new JsonSerializerSettings { ContractResolver = new IgnoreAttachmentsResolver() };
+        }
+
         public byte[] Serialize<T>(T message)
         {
-            var serialized = JsonConvert.SerializeObject(message);
+            var serialized = JsonConvert.SerializeObject(message, _settings);
             return Encoding.UTF8.GetBytes(serialized);
         }
 
         public T Deserialize<T>(ReadOnlySpan<byte> serialized)
         {
-            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(serialized));
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(serialized), _settings);
         }
 
         public T Deserialize<T>(ReadOnlyMemory<byte> serialized)
         {
-            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(serialized.Span));
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(serialized.Span), _settings);
         }
 
         public Task<T> Deserialize<T>(Stream serialized)
@@ -35,5 +44,19 @@ namespace KnightBus.Newtonsoft
         }
 
         public string ContentType => "application/json";
+    }
+
+    public class IgnoreAttachmentsResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var prop = base.CreateProperty(member, memberSerialization);
+            if (typeof(ICommandWithAttachment).IsAssignableFrom(member.DeclaringType) && member.Name == nameof(ICommandWithAttachment.Attachment))
+            {
+                prop.Ignored = true;
+            }
+
+            return prop;
+        }
     }
 }
--- knightbus/tests/KnightBus.Core.Tests.Unit/JsonSerializerTests.cs
+++ knightbus/tests/KnightBus.Core.Tests.Unit/JsonSerializerTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using FluentAssertions;
 using KnightBus.Messages;
+using KnightBus.Newtonsoft;
 using NUnit.Framework;
 
 namespace KnightBus.Core.Tests.Unit
@@ -32,6 +33,44 @@ namespace KnightBus.Core.Tests.Unit
         {
             //arrange
             var serializer = new MicrosoftJsonSerializer();
+            var original = new TestSerializationCommand
+            {
+                Age = 1,
+                Height = 5.7f,
+                Name = "Aurora"
+            };
+            var serialized = serializer.Serialize(original);
+            //act
+            var deserialized = serializer.Deserialize<TestSerializationCommand>(serialized.AsSpan());
+            //assert
+            deserialized.Age.Should().Be(original.Age);
+            deserialized.Name.Should().Be(original.Name);
+            deserialized.Height.Should().Be(original.Height);
+        }
+
+        [Test]
+        public void Newtonsoft_Should_not_serialize_attachments()
+        {
+            //arrange
+            var serializer = new NewtonsoftSerializer();
+            var message = new AttachmentCommand
+            {
+                Message = "Hello",
+                Attachment = new MessageAttachment("filename.txt", "text/plain", new MemoryStream())
+            };
+            //act
+            var serialized = serializer.Serialize(message);
+            var deserialized = serializer.Deserialize<AttachmentCommand>(serialized.AsSpan());
+            //assert
+            deserialized.Attachment.Should().BeNull();
+            deserialized.Message.Should().Be("Hello");
+        }
+
+        [Test]
+        public void Newtonsoft_Should_serialize_deserialize_message()
+        {
+            //arrange
+            var serializer = new NewtonsoftSerializer();
             var original = new TestSerializationCommand
             {
                 Age = 1,
