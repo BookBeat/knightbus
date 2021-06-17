1ef018d359f406c5a105791e204f92b2ece8460e
Magnus Baneryd <magnus.baneryd@bookbeat.com>
2021-06-17 20:34:20 +0200

    Nats queue subscribe/publish

---
+++ knightbus-nats/src/KnightBus.Nats/NatsBusMessageStateHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KnightBus.Core;
+using KnightBus.Messages;
+using NATS.Client;
+
+namespace KnightBus.Nats
+{
+    internal class NatsBusMessageStateHandler<T> : IMessageStateHandler<T> where T : class, IMessage
+    {
+        private readonly T _message;
+        private readonly MsgHandlerEventArgs _processMessage;
+
+        public NatsBusMessageStateHandler(MsgHandlerEventArgs processMessage, IMessageSerializer serializer, int deadLetterDeliveryLimit, IDependencyInjection messageScope)
+        {
+            DeadLetterDeliveryLimit = deadLetterDeliveryLimit;
+            MessageScope = messageScope;
+            _processMessage = processMessage;
+            _message = serializer.Deserialize<T>(new ReadOnlyMemory<byte>(processMessage.Message.Data));
+        }
+
+        public int DeliveryCount { get; } = 1;
+        public int DeadLetterDeliveryLimit { get; }
+        public IDictionary<string, string> MessageProperties { get; }
+
+        public Task CompleteAsync()
+        {
+            _processMessage.Message.Respond(null);
+            return Task.CompletedTask;
+        }
+
+        public Task AbandonByErrorAsync(Exception e)
+        {
+            // await _sbMessage.AbandonByErrorAsync(_processMessage, e).ConfigureAwait(false);
+            return Task.CompletedTask;
+        }
+
+        public Task DeadLetterAsync(int deadLetterLimit)
+        {
+            // await _sbMessage.DeadLetterByDeliveryLimitAsync(_processMessage, deadLetterLimit).ConfigureAwait(false);
+            return Task.CompletedTask;
+        }
+        
+        public Task<T> GetMessageAsync()
+        {
+            return Task.FromResult(_message);
+        }
+
+        public IDependencyInjection MessageScope { get; set; }
+    }
+}
---
+++ knightbus-nats/src/KnightBus.Nats/NatsTransport.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using KnightBus.Core;
+using KnightBus.Messages;
+using KnightBus.Newtonsoft;
+
+namespace KnightBus.Nats
+{
+    public class NatsTransport : ITransport
+    {
+        public NatsTransport(string connectionString)
+            : this(new NatsBusConfiguration(connectionString))
+        {
+            
+        }
+        public NatsTransport(INatsBusConfiguration configuration)
+        {
+            TransportChannelFactories = new ITransportChannelFactory[] {new NatsChannelFactory(configuration),};
+        }
+        public ITransportChannelFactory[] TransportChannelFactories { get; }
+        public ITransport ConfigureChannels(ITransportConfiguration configuration)
+        {
+            foreach (var channelFactory in TransportChannelFactories)
+            {
+                channelFactory.Configuration = configuration;
+            }
+
+            return this;
+        }
+
+        public ITransport UseMiddleware(IMessageProcessorMiddleware middleware)
+        {
+            foreach (var channelFactory in TransportChannelFactories)
+            {
+                channelFactory.Middlewares.Add(middleware);
+            }
+
+            return this;
+        }
+    }
+
+    public class NatsChannelFactory : ITransportChannelFactory
+    {
+        public NatsChannelFactory(INatsBusConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public ITransportConfiguration Configuration { get; set; }
+        public IList<IMessageProcessorMiddleware> Middlewares { get; } = new List<IMessageProcessorMiddleware>();
+
+        public IChannelReceiver Create(Type messageType, IEventSubscription subscription, IProcessingSettings processingSettings,
+            IMessageSerializer serializer, IHostConfiguration configuration, IMessageProcessor processor)
+        {
+
+            var readerType = typeof(NatsQueueChannelReceiver<>).MakeGenericType(messageType);
+            var reader = (IChannelReceiver) Activator.CreateInstance(readerType, processingSettings, serializer,
+                configuration, processor, Configuration);
+
+            return reader;
+        }
+
+        public bool CanCreate(Type messageType)
+        {
+            return typeof(INatsCommand).IsAssignableFrom(messageType);
+        }
+    }
+    
+    public interface INatsBusConfiguration : ITransportConfiguration
+    {
+    }
+
+    public class NatsBusConfiguration : INatsBusConfiguration
+    {
+        public NatsBusConfiguration(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public string ConnectionString { get; }
+        public IMessageSerializer MessageSerializer { get; set; } = new NewtonsoftSerializer();
+    }
+
+    public interface INatsCommand : ICommand
+    {
+        
+    }
+}
