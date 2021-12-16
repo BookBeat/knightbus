ea686b770db4d11b7d1d0b4b0d07b135dcaa9a96
Niklas Arbin <niklas.arbin@bookbeat.com>
2021-12-16 11:17:15 +0100

    net 6 syntax removal

--- knightbus/src/KnightBus.Core/IProcessStreamRequest.cs
+++ knightbus/src/KnightBus.Core/IProcessStreamRequest.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using KnightBus.Messages;
 
-namespace KnightBus.Core;
-
-public interface IProcessStreamRequest<TRequest, TResponse, TSettings> : IProcessMessage<TRequest, IAsyncEnumerable<TResponse>>
-    where TRequest : IRequest
-    where TSettings : class, IProcessingSettings, new()
+namespace KnightBus.Core
 {
+    public interface
+        IProcessStreamRequest<TRequest, TResponse, TSettings> : IProcessMessage<TRequest, IAsyncEnumerable<TResponse>>
+        where TRequest : IRequest
+        where TSettings : class, IProcessingSettings, new()
+    {
 
+    }
 }
