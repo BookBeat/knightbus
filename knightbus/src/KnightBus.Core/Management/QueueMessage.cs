7c5f997366fce5416da8ad5a745fb20b78d9c737
Niklas Arbin <niklas.arbin@bookbeat.com>
2024-03-22 12:48:37 +0100

    feat: introduce manager into core so implementations can be displayed alongside

---
+++ knightbus/src/KnightBus.Core/Management/QueueMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightBus.Core.Management;
+
+public record QueueMessage(
+    string Body,
+    string Error,
+    DateTimeOffset? Time,
+    int DeliveryCount,
+    string MessageId,
+    IReadOnlyDictionary<string, string> Properties)
+{
+    public QueueMessage(string Body,
+        string Error,
+        DateTimeOffset? Time,
+        int DeliveryCount,
+        string MessageId,
+        IReadOnlyDictionary<string, object> Properties) : this(Body, Error, Time, DeliveryCount, MessageId, Properties.ToDictionary(x => x.Key, x => x.Value.ToString()))
+    { }
+};
---
+++ knightbus/src/KnightBus.Core/Management/SubscriptionQueueProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace KnightBus.Core.Management;
+
+public class SubscriptionQueueProperties : QueueProperties
+{
+    public SubscriptionQueueProperties(string name, IQueueManager manager, string topic, bool isLoaded) : base(name, manager, isLoaded, QueueType.Subscription)
+    {
+        Topic = topic;
+    }
+
+    public string Topic { get; }
+}
