f1ec8443a505925135eb98d6310ac7cea243f050
niklasarbin <nikls.arbin@bookbeat.com>
2024-03-28 11:48:29 +0100

    feat: expose db id in redis extension

--- knightbus-redis/src/KnightBus.Redis.Management/RedisQueueExtensions.cs
+++ knightbus-redis/src/KnightBus.Redis.Management/RedisQueueExtensions.cs
@@ -5,13 +5,17 @@ namespace KnightBus.Redis.Management;
 
 public static class RedisQueueExtensions
 {
-    public static IServiceCollection UseRedisManagement(this IServiceCollection services, string connectionString)
+    public static IServiceCollection UseRedisManagement(this IServiceCollection services, string connectionString, int databaseId = 0)
     {
         services = services
             .AddScoped<RedisQueueManager>()
             .AddScoped<IQueueManager, RedisQueueManager>()
             .AddScoped<IRedisManagementClient, RedisManagementClient>();
 
-        return services.UseRedis(c => c.ConnectionString = connectionString);
+        return services.UseRedis(c =>
+        {
+            c.ConnectionString = connectionString;
+            c.DatabaseId = databaseId;
+        });
     }
 }
