e1522100eccfce158e6cbf956e48ed34984e9101
Tobias Balzano <Tobias.balzano@bookbeat.com>
2024-06-04 13:18:24 +0200

    Add some helpful health checking instrumentation

---
+++ knightbus-health/src/KnightBus.Health/RemediationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KnightBus.Health;
+
+public static class RemediationExtensions
+{
+    public static string GetRemediationText<T>(this Type command)
+    {
+        var remediationMapping = command.GetInterface(nameof(IRemediationMapping));
+        if (remediationMapping == null)
+            return "";
+
+        var remediationMessageProperty = remediationMapping.GetProperty(
+            nameof(IRemediationMapping.RemediationMessage)
+        );
+        if (remediationMessageProperty == null)
+            return "";
+
+        var remediationText = remediationMessageProperty.GetValue(command) as string;
+        if (remediationText == null)
+            return "";
+
+        return remediationText;
+    }
+}
