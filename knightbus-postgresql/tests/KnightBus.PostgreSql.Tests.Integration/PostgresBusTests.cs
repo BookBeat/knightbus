58cd662a2a5775537fe44f87f5b4cc5a34623e5d
Johannes Jörbrand <johannes.j@outlook.com>
2024-08-28 15:49:35 +0200

    feat: implement postgres deadletter counter

--- knightbus-postgresql/tests/KnightBus.PostgreSql.Tests.Integration/PostgresBusTests.cs
+++ knightbus-postgresql/tests/KnightBus.PostgreSql.Tests.Integration/PostgresBusTests.cs
@@ -19,34 +19,45 @@ public class PostgresBusTests
     public async Task OneTimeSetUp()
     {
         _postgresBus = new PostgresBus(PostgresSetup.DataSource, new PostgresConfiguration());
-        _postgresQueueClient = new PostgresQueueClient<TestCommand>(PostgresSetup.DataSource, new MicrosoftJsonSerializer());
-        _postgresManagementClient = new PostgresManagementClient(PostgresSetup.DataSource,
-            new PostgresConfiguration { MessageSerializer = new MicrosoftJsonSerializer() });
+        _postgresQueueClient = new PostgresQueueClient<TestCommand>(
+            PostgresSetup.DataSource,
+            new MicrosoftJsonSerializer()
+        );
+        _postgresManagementClient = new PostgresManagementClient(
+            PostgresSetup.DataSource,
+            new PostgresConfiguration { MessageSerializer = new MicrosoftJsonSerializer() }
+        );
         await QueueInitializer.InitQueue(
             PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()),
-            PostgresSetup.DataSource);
+            PostgresSetup.DataSource
+        );
     }
 
     [OneTimeTearDown]
     public async Task OneTimeTearDown()
     {
         await _postgresManagementClient.DeleteQueue(
-            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()), default);
+            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()),
+            default
+        );
     }
 
     [SetUp]
     public async Task SetUp()
     {
         await _postgresManagementClient.PurgeQueue(
-            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()));
+            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>())
+        );
         await _postgresManagementClient.PurgeDeadLetterQueue(
-            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()));
+            PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>())
+        );
     }
 
     [Test]
     public async Task GetMessages_Empty()
     {
-        var messages = _postgresQueueClient.GetMessagesAsync(1, 100, default)
+        var messages = _postgresQueueClient
+            .GetMessagesAsync(1, 100, default)
             .ToBlockingEnumerable()
             .ToList();
         messages.Count.Should().Be(0);
@@ -56,16 +67,20 @@ public class PostgresBusTests
     public async Task InsertMessages()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "hello, world!" },
-            new TestCommand { MessageBody = "hello?!" }
-        ], default);
+            [
+                new TestCommand { MessageBody = "hello, world!" },
+                new TestCommand { MessageBody = "hello?!" }
+            ],
+            default
+        );
 
-        var messagesCount = (long)
-            (await PostgresSetup.DataSource
-                .CreateCommand(
-                    $"SELECT COUNT(*) FROM knightbus.q_{AutoMessageMapper.GetQueueName<TestCommand>()};")
-                .ExecuteScalarAsync() ?? 0);
+        var messagesCount = (long)(
+            await PostgresSetup
+                .DataSource.CreateCommand(
+                    $"SELECT COUNT(*) FROM knightbus.q_{AutoMessageMapper.GetQueueName<TestCommand>()};"
+                )
+                .ExecuteScalarAsync() ?? 0
+        );
 
         messagesCount.Should().Be(2);
     }
@@ -74,12 +89,15 @@ public class PostgresBusTests
     public async Task GetMessages()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "message body 1" },
-            new TestCommand { MessageBody = "message body 2" }
-        ], default);
+            [
+                new TestCommand { MessageBody = "message body 1" },
+                new TestCommand { MessageBody = "message body 2" }
+            ],
+            default
+        );
 
-        var messages = _postgresQueueClient.GetMessagesAsync(2, 100, default)
+        var messages = _postgresQueueClient
+            .GetMessagesAsync(2, 100, default)
             .ToBlockingEnumerable()
             .ToList();
 
@@ -94,13 +112,16 @@ public class PostgresBusTests
     public async Task GetMessages_visibility_timeout()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "message body 1" },
-            new TestCommand { MessageBody = "message body 2" }
-        ], default);
+            [
+                new TestCommand { MessageBody = "message body 1" },
+                new TestCommand { MessageBody = "message body 2" }
+            ],
+            default
+        );
 
         // fetch latest 2 messages
-        var messages1 = _postgresQueueClient.GetMessagesAsync(2, 100, default)
+        var messages1 = _postgresQueueClient
+            .GetMessagesAsync(2, 100, default)
             .ToBlockingEnumerable()
             .ToList();
         messages1.Count.Should().Be(2);
@@ -108,7 +129,8 @@ public class PostgresBusTests
         await Task.Delay(3000);
 
         // fetch latest 2 messages again
-        var messages2 = _postgresQueueClient.GetMessagesAsync(2, 100, default)
+        var messages2 = _postgresQueueClient
+            .GetMessagesAsync(2, 100, default)
             .ToBlockingEnumerable()
             .ToList();
         messages2.Count.Should().Be(0);
@@ -118,22 +140,27 @@ public class PostgresBusTests
     public async Task Complete()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "delete me" },
-        ], default);
+            [new TestCommand { MessageBody = "delete me" },],
+            default
+        );
 
-        var message = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var message = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
 
         await _postgresQueueClient.CompleteAsync(message[0]);
 
         var deleted = (long)
-            (await PostgresSetup.DataSource
-                .CreateCommand(@$"
+            (
+                await PostgresSetup
+                    .DataSource.CreateCommand(
+                        @$"
 SELECT COUNT(*) FROM knightbus.q_{AutoMessageMapper.GetQueueName<TestCommand>()}
-WHERE message_id = {message[0].Id}")
-                .ExecuteScalarAsync())!;
+WHERE message_id = {message[0].Id}"
+                    )
+                    .ExecuteScalarAsync()
+            )!;
 
         deleted.Should().Be(0);
     }
@@ -142,17 +169,22 @@ WHERE message_id = {message[0].Id}")
     public async Task AbandonByError()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "abandon me" },
-        ], default);
+            [new TestCommand { MessageBody = "abandon me" },],
+            default
+        );
 
-        var message = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var message = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
 
-        await _postgresQueueClient.AbandonByErrorAsync(message[0], new Exception("some error message"));
+        await _postgresQueueClient.AbandonByErrorAsync(
+            message[0],
+            new Exception("some error message")
+        );
 
-        var result = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var result = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
         result[0].ReadCount.Should().Be(2);
@@ -163,29 +195,42 @@ WHERE message_id = {message[0].Id}")
     public async Task DeadLetterMessage()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "dead letter me" }
-        ], default);
+            [new TestCommand { MessageBody = "dead letter me" }],
+            default
+        );
 
-        var message = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var message = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
         await _postgresQueueClient.DeadLetterMessageAsync(message[0]);
 
         var originalMessage = (long)
-            (await PostgresSetup.DataSource
-                .CreateCommand(@$"
+            (
+                await PostgresSetup
+                    .DataSource.CreateCommand(
+                        @$"
 SELECT COUNT(*) FROM knightbus.q_{AutoMessageMapper.GetQueueName<TestCommand>()}
-WHERE message_id = {message[0].Id}")
-                .ExecuteScalarAsync())!;
+WHERE message_id = {message[0].Id}"
+                    )
+                    .ExecuteScalarAsync()
+            )!;
 
         originalMessage.Should().Be(0);
 
-        var deadLetters = _postgresManagementClient.PeekDeadLettersAsync(
-                PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()), 10, default)
+        var deadLetters = _postgresManagementClient
+            .PeekDeadLettersAsync(
+                PostgresQueueName.Create(AutoMessageMapper.GetQueueName<TestCommand>()),
+                10,
+                default
+            )
             .ToBlockingEnumerable()
             .ToList();
-        deadLetters[0].Message["MessageBody"].ToString().Should().BeEquivalentTo(message[0].Message.MessageBody);
+        deadLetters[0]
+            .Message["MessageBody"]
+            .ToString()
+            .Should()
+            .BeEquivalentTo(message[0].Message.MessageBody);
         deadLetters[0].Id.Should().Be(message[0].Id);
     }
 
@@ -193,20 +238,24 @@ WHERE message_id = {message[0].Id}")
     public async Task Schedule()
     {
         await _postgresBus.ScheduleAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "for future" }
-        ], TimeSpan.FromSeconds(3), default);
+            [new TestCommand { MessageBody = "for future" }],
+            TimeSpan.FromSeconds(3),
+            default
+        );
 
-        var messages = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var messages = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
         messages.Count.Should().Be(0);
 
         await Task.Delay(3000);
 
-        var result = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var result = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
-            .ToList();;
+            .ToList();
+        ;
         result[0].Message.MessageBody.Should().Be("for future");
     }
 
@@ -214,22 +263,25 @@ WHERE message_id = {message[0].Id}")
     public async Task PeekDeadLetterMessagesAsync()
     {
         await _postgresBus.SendAsync<TestCommand>(
-        [
-            new TestCommand { MessageBody = "dead letter" }
-        ], default);
+            [new TestCommand { MessageBody = "dead letter" }],
+            default
+        );
 
-        var message = _postgresQueueClient.GetMessagesAsync(1, 10, default)
+        var message = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
             .ToBlockingEnumerable()
             .ToList();
         await _postgresQueueClient.DeadLetterMessageAsync(message[0]);
 
-        var firstResult = _postgresQueueClient.PeekDeadLetterMessagesAsync(1, default)
+        var firstResult = _postgresQueueClient
+            .PeekDeadLetterMessagesAsync(1, default)
             .ToBlockingEnumerable()
             .ToList();
         firstResult.Single().Id.Should().Be(message[0].Id);
         firstResult.Single().Message.Should().BeEquivalentTo(message[0].Message);
 
-        var secondResult = _postgresQueueClient.PeekDeadLetterMessagesAsync(1, default)
+        var secondResult = _postgresQueueClient
+            .PeekDeadLetterMessagesAsync(1, default)
             .ToBlockingEnumerable()
             .ToList();
         secondResult.Single().Id.Should().Be(message[0].Id);
@@ -237,14 +289,48 @@ WHERE message_id = {message[0].Id}")
     }
 
     [Test]
+    public async Task GetDeadLetterMessageCountAsync_ShouldReturn1_WhenDeadletterInQueue()
+    {
+        await _postgresBus.SendAsync<TestCommand>(
+            [new TestCommand { MessageBody = "dead letter" }],
+            default
+        );
+
+        var message = _postgresQueueClient
+            .GetMessagesAsync(1, 10, default)
+            .ToBlockingEnumerable()
+            .ToList();
+        await _postgresQueueClient.DeadLetterMessageAsync(message[0]);
+
+        var result = await _postgresQueueClient.GetDeadLetterMessageCountAsync(default);
+        result.Should().Be(1);
+    }
+
+    [Test]
+    public async Task GetDeadLetterMessageCountAsync_ShouldReturn0_WhenNoDeadletterInQueue()
+    {
+        await _postgresBus.SendAsync<TestCommand>(
+            [new TestCommand { MessageBody = "hello, world!" }],
+            default
+        );
+
+        var result = await _postgresQueueClient.GetDeadLetterMessageCountAsync(default);
+        result.Should().Be(0);
+    }
+
+    [Test]
     public async Task ManagementClient_SendMessages()
     {
         var message = new { MessageBody = "hello, world!" };
         var jsonBody = JsonSerializer.Serialize(message);
-        await _postgresManagementClient.SendMessage(PostgresQueueName.Create("my_queue"),
-            jsonBody, default);
+        await _postgresManagementClient.SendMessage(
+            PostgresQueueName.Create("my_queue"),
+            jsonBody,
+            default
+        );
 
-        var messages = _postgresQueueClient.GetMessagesAsync(1, 100, default)
+        var messages = _postgresQueueClient
+            .GetMessagesAsync(1, 100, default)
             .ToBlockingEnumerable()
             .ToList();
 
