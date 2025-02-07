6f55564bea2552023c7c9e35df49f7aa67dd5a2d
Gustaf Larsson <gustaf.larsson01@gmail.com>
2025-02-07 14:43:58 +0100

    updated naming

--- KnightBus.CosmosDB/CosmosBus.cs
+++
@@ -1,7 +0,0 @@
-﻿using KnightBus.Cosmos.Messages;
-
-public interface ICosmosBus
-{
-    Task SendAsync<T>(T message, CancellationToken ct) where T : ICosmosCommand;
-    //More things should be added later
-}
---
+++ knightbus-cosmos/src/KnightBus.Cosmos/CosmosBus.cs
@@ -0,0 +1,70 @@
+﻿using KnightBus.Cosmos.Messages;
+
+namespace KnightBus.Cosmos;
+public interface ICosmosBus
+{
+    
+    //Send single command
+    Task SendAsync<T>(T message, CancellationToken ct) where T : ICosmosCommand;
+    
+    //Send Multiple commands
+    Task SendAsync<T>(IEnumerable<T> messages, CancellationToken ct) where T : ICosmosCommand;
+    
+    //Publish one event
+    Task PublishAsync<T>(T message, CancellationToken ct) where T : ICosmosEvent;
+    
+    //Publish multiple events
+    Task PublishAsync<T>(IEnumerable<T> messages, CancellationToken ct) where T : ICosmosEvent;
+    
+    //Schedule one command
+    Task ScheduleAsync<T>(T message, TimeSpan delay, CancellationToken ct) where T : ICosmosCommand;
+    
+    //Schedule multiple commands
+    Task ScheduleAsync<T>(IEnumerable<T> messages, TimeSpan delay, CancellationToken ct) where T : ICosmosCommand;
+}
+
+public class CosmosBus : ICosmosBus
+{
+    //Send a single command
+    public Task SendAsync<T>(T message, CancellationToken ct) where T : ICosmosCommand
+    {
+        //To be implemented
+        return Task.CompletedTask;
+    }
+
+    //Send multiple commands
+    public Task SendAsync<T>(IEnumerable<T> messages, CancellationToken ct) where T : ICosmosCommand
+    {
+        //To be implemented
+        return Task.CompletedTask;
+    }
+
+    //Publish a single event
+    public Task PublishAsync<T>(T message, CancellationToken ct) where T : ICosmosEvent
+    {
+        return Task.CompletedTask;
+    }
+    
+    //Publish multiple events
+    public Task PublishAsync<T>(IEnumerable<T> messages, CancellationToken ct) where T : ICosmosEvent
+    {
+        //To be implemented
+        return Task.CompletedTask;
+    }
+
+    //Schedule a single command
+    public Task ScheduleAsync<T>(T message, TimeSpan delay, CancellationToken ct) where T : ICosmosCommand
+    {
+        //To be implemented
+        return Task.CompletedTask;
+    }
+
+    //Schedule multiple commands
+    public Task ScheduleAsync<T>(IEnumerable<T> messages, TimeSpan delay, CancellationToken ct) where T : ICosmosCommand
+    {
+        //To be implemented
+        return Task.CompletedTask;
+    }
+}
+
+
