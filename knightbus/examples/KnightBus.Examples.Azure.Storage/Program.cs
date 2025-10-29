fffdd48a8e29f1d5e5e00091f0a987dc335d0d4a
Axel Elmarsson <axel.elmarsson@bookbeat.com>
2025-10-29 14:18:58 +0100

    fix

--- knightbus/examples/KnightBus.Examples.Azure.Storage/Program.cs
+++ knightbus/examples/KnightBus.Examples.Azure.Storage/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Identity;
 using KnightBus.Azure.Storage;
 using KnightBus.Azure.Storage.Messages;
 using KnightBus.Core;
