713eae6fa477b1d10363f014205e9001ee3636eb
Axel Elmarsson <axel.elmarsson@bookbeat.com>
2025-12-16 12:24:56 +0100

    feat: allow processors to share lock instance

---
+++ knightbus-schedule/tests/KnightBus.Schedule.Tests.Unit/ScheduleProcessorRegistrationTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using KnightBus.Core.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace KnightBus.Schedule.Tests.Unit;
+
+public class ScheduleProcessorRegistrationTests
+{
+    [Test]
+    public void Enumerating_processors_for_shared_schedule_returns_all_registrations()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services
+            .RegisterSchedule<SharedScheduleProcessorOne, SharedSchedule>()
+            .RegisterSchedule<SharedScheduleProcessorTwo, SharedSchedule>()
+            .RegisterSchedule<IndependentScheduleProcessor, IndependentSchedule>();
+        using var provider = services.BuildServiceProvider(
+            new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true }
+        );
+        using var scope = provider.CreateScope();
+
+        // Act
+        var processors = scope
+            .ServiceProvider.GetServices<IProcessSchedule<SharedSchedule>>()
+            .Select(p => p.GetType())
+            .ToList();
+
+        // Assert
+        processors
+            .Should()
+            .BeEquivalentTo([
+                typeof(SharedScheduleProcessorOne),
+                typeof(SharedScheduleProcessorTwo),
+            ]);
+    }
+
+    [Test]
+    public void Open_generic_schedule_lookup_returns_each_concrete_processor()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services
+            .RegisterSchedule<SharedScheduleProcessorOne, SharedSchedule>()
+            .RegisterSchedule<SharedScheduleProcessorTwo, SharedSchedule>()
+            .RegisterSchedule<IndependentScheduleProcessor, IndependentSchedule>();
+        using var provider = services.BuildServiceProvider(
+            new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true }
+        );
+        using var di = new MicrosoftDependencyInjection(provider);
+
+        // Act
+        var registrations = di.GetOpenGenericRegistrations(typeof(IProcessSchedule<>)).ToList();
+
+        // Assert
+        registrations
+            .Should()
+            .BeEquivalentTo([
+                typeof(SharedScheduleProcessorOne),
+                typeof(SharedScheduleProcessorTwo),
+                typeof(IndependentScheduleProcessor),
+            ]);
+    }
+
+    private record SharedSchedule : ISchedule
+    {
+        public string CronExpression => "0 */5 * ? * *";
+        public TimeZoneInfo TimeZone => TimeZoneInfo.Utc;
+    }
+
+    private record IndependentSchedule : ISchedule
+    {
+        public string CronExpression => "0 */10 * ? * *";
+        public TimeZoneInfo TimeZone => TimeZoneInfo.Utc;
+    }
+
+    private class SharedScheduleProcessorOne : IProcessSchedule<SharedSchedule>
+    {
+        public Task ProcessAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    private class SharedScheduleProcessorTwo : IProcessSchedule<SharedSchedule>
+    {
+        public Task ProcessAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    private class IndependentScheduleProcessor : IProcessSchedule<IndependentSchedule>
+    {
+        public Task ProcessAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- knightbus/examples/KnightBus.Examples.Schedule/Program.cs
+++ knightbus/examples/KnightBus.Examples.Schedule/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using KnightBus.Azure.Storage;
+using KnightBus.Core.Singleton;
 using KnightBus.Host;
 using KnightBus.Schedule;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace KnightBus.Examples.Schedule;
 
@@ -19,7 +23,7 @@ public class Program
     {
         var blobConnection = "UseDevelopmentStorage=true";
 
-        var knightBus = global::Microsoft
+        var knightBus = Microsoft
             .Extensions.Hosting.Host.CreateDefaultBuilder()
             .UseDefaultServiceProvider(options =>
             {
@@ -33,7 +37,8 @@ public class Program
                     .UseScheduling()
                     .UseTcpAliveListener(13000)
                     .RegisterSchedules()
-                    .UseBlobStorageLockManager();
+                    .AddSingleton<ISingletonLockManager, InMemoryLockManager>();
+                // .UseBlobStorageLockManager();
             })
             .UseKnightBus()
             .Build();
@@ -59,6 +64,56 @@ public class MySchedule : IProcessSchedule<EveryMinute>, IProcessSchedule<EveryM
     public Task ProcessAsync(CancellationToken cancellationToken)
     {
         Console.WriteLine("Schedule triggered!");
+        return Task.CompletedTask;
+    }
+}
+
+public class InMemoryLockManager : ISingletonLockManager
+{
+    private readonly ConcurrentDictionary<string, int> _heldLocks = new();
+
+    public Task<ISingletonLockHandle> TryLockAsync(
+        string lockId,
+        TimeSpan lockPeriod,
+        CancellationToken cancellationToken
+    )
+    {
+        if (_heldLocks.TryAdd(lockId, 0))
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(50));
+                _heldLocks.TryRemove(lockId, out _);
+            });
+            return Task.FromResult<ISingletonLockHandle>(new InMemoryLockHandle(lockId));
+        }
+
+        return Task.FromResult<ISingletonLockHandle>(null);
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class InMemoryLockHandle : ISingletonLockHandle
+{
+    public string LeaseId => LockId;
+    public string LockId { get; }
+
+    public InMemoryLockHandle(string lockId)
+    {
+        LockId = lockId;
+    }
+
+    public Task<bool> RenewAsync(ILogger log, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task ReleaseAsync(CancellationToken cancellationToken)
+    {
         return Task.CompletedTask;
     }
 }
